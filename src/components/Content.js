eb69bdc714208fd4251aba328bd236295e0fcd78
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-06-05 08:03:21 -0600

    setup file structure for all required components

---
+++ src/components/Content.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+import React, { useState } from "react";
+import Navigation from './Navigation';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function Content() {
+    const [currentPage, setCurrentPage] = useState("ABOUT");
+
+    const renderPage = () => {
+        if (currentPage === "PORTFOLIO") {
+            return <Portfolio />;
+        }
+        if (currentPage === "CONTACT") {
+            return <Contact />;
+        }
+        if (currentPage === "RESUME") {
+            return <Resume />;
+        }
+        return <About />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {/* Pass the currentPage from state and the function to update it */}
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+            {/* Call the render page function to return a component */}
+            {renderPage()}
+        </div>
+    );
+}
